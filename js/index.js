82d035a68a54619b8f0f6a2fa4b2505676f98863
debeldami <44111962+debeldami@users.noreply.github.com>
2020-01-31 15:23:16 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var num1 = Math.floor(Math.random()*6) + 1;
+var num2 = Math.floor(Math.random()*6) + 1;
+
+var win = document.querySelector("h1");
+var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
+
+if(num1 > num2){
+    win.textContent = "player one win";
+}else if(num2 > num1){
+    win.textContent = "player two win";
+}else{
+    win.textContent = "draw";
+}
+
+var dice1 = "images/dice"+num1+".png";
+var dice2 = "images/dice"+num2+".png";
+console.log(dice1);
+console.log(dice2);
+
+img1.setAttribute("src",dice1);
+img2.setAttribute("src",dice2);
